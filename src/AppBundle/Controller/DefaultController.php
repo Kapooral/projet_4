a3f6cf2461b3790bef158b91e7d1d8f3df2b7aa7
unknown <mbenguia.husseini@live.fr>
2018-04-20 05:12:04 +0200

    Sécurisation de la page de confirmation + Ajout de la gestion des autres exceptions Stripe lors du paiement.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -33,7 +33,6 @@ class DefaultController extends Controller
     			$order->addTicket($ticket);
     		}
 
-    		$order->setOrderCode(bin2hex(random_bytes(5)));
 			$request->getSession()->set('order', $order);
 
     		return $this->redirectToRoute('app_info');
@@ -44,10 +43,9 @@ class DefaultController extends Controller
 
     public function infoAction(Request $request)
     {
-    	if(!$request->getSession()->has('order') || $request->getSession()->get('order')->getOrderCode() == null)
+    	if(!$request->getSession()->has('order'))
     	{
-    		$request->getSession()->clear();
-    		return $this->redirectToRoute('app_booking');
+    		return $this->redirectToRoute('app_index');
     	}
 
     	$form = $this->createForm(OrderChildType::class, $request->getSession()->get('order'));
@@ -57,7 +55,7 @@ class DefaultController extends Controller
     		if(!$form->get('cgv')->getData())
     		{
     			$request->getSession()->clear();
-    			return $this->redirectToRoute('app_booking');
+    			return $this->redirectToRoute('app_index');
     		}
     		else
     		{
@@ -70,10 +68,9 @@ class DefaultController extends Controller
 
     public function summaryAction(Request $request)
     {
-    	if(!$request->getSession()->has('order') || $request->getSession()->get('order')->getOrderCode() == null)
+    	if(!$request->getSession()->has('order'))
     	{
-    		$request->getSession()->clear();
-    		return $this->redirectToRoute('app_booking');
+    		return $this->redirectToRoute('app_index');
     	}
     	elseif($request->getSession()->get('order')->getEmail() == null)
     	{
@@ -87,18 +84,20 @@ class DefaultController extends Controller
 
     		try
     		{
-	    		\Stripe\Charge::create(array(
+	    		$charge = \Stripe\Charge::create(array(
 	    			"amount" => $totalPrice,
 	    			"currency" => 'eur',
 	    			"description" => 'Billet(s) Musée du Louvre',
 	    			"source" => $request->request->get('stripeToken')
 	    		));
     		} 
-    		catch (\Stripe\Error\Card $e)
+    		catch (\Stripe\Error\Card | \Stripe\Error\RateLimit | \Stripe\Error\InvalidRequest | \Stripe\Error\Authentication | \Stripe\Error\ApiConnection | \Stripe\Error\Base | Exception $e)
     		{
+                $message = 'Une erreur est survenue. Votre paiement n\'a pas été effectué, veuillez recommencer.';
     			return $this->redirectToRoute('app_summary');
     		}
 
+            $request->getSession()->get('order')->setOrderCode($charge->id);
     		return $this->redirectToRoute('app_confirmation');
     	}
 
@@ -107,6 +106,32 @@ class DefaultController extends Controller
 
     public function confirmationAction(Request $request)
     {
+        if(!$request->getSession()->has('order'))
+        {
+            return $this->redirectToRoute('app_index');
+        }
+        elseif($request->getSession()->get('order')->getOrderCode() == null)
+        {
+            return $this->redirectToRoute('app_summary');
+        }
+
+        try
+        {
+            \Stripe\Stripe::setApiKey('sk_test_Ce6DBxnu8smTNyzF2TokYRIO');
+            $charge = \Stripe\Charge::retrieve($request->getSession()->get('order')->getOrderCode());
+        }
+        catch(\Stripe\Error\InvalidRequest $e)
+        {
+            $message = 'Une erreur est survenue. Votre paiement n\'a pas été effectué, veuillez recommencer.';
+            return $this->redirectToRoute('app_summary');
+        }
+        
+        if(!$charge->status == "succeeded")
+        {
+            $message = 'Une erreur est survenue. Votre paiement n\'a pas été effectué, veuillez recommencer.';
+            return $this->redirectToRoute('app_summary');
+        }
+
     	$em = $this->getDoctrine()->getManager();
     	$em->persist($request->getSession()->get('order'));
     	$em->flush();
